70416ca2fca25f24d09c1091073e69f61fed6ea2
Sara Bauer <saradel018@gmail.com>
2023-02-23 17:08:40 -0600

    Successful creation of usable DD dataframe. Commented out SRP.df code. Created code to add metadata columns for models.

--- Degree_day_linear_models_V2.R
+++ Degree_day_linear_models_V2.R
@@ -34,15 +34,15 @@ dailymeans.df <- dailymeans.df %>% filter(meantemp > 0)
 #setting up site,rep,position (SRP) names for easier grouping.
 
 
-SRP.df <- as_tibble(str_split_fixed(dailymeans.df$name, "_", 5))
-colnames(SRP.df) <- c("site", "rep", "position", "buttonID", "season")
-head(SRP.df)
-SRP.df$SRP.name <- paste(SRP.df$site, SRP.df$rep, SRP.df$position, sep = "_")
-head(SRP.df)
+#SRP.df <- as_tibble(str_split_fixed(dailymeans.df$name, "_", 5))
+#colnames(SRP.df) <- c("site", "rep", "position", "buttonID", "season")
+#head(SRP.df)
+#SRP.df$SRP.name <- paste(SRP.df$site, SRP.df$rep, SRP.df$position, sep = "_")
+#head(SRP.df)
 
-dailymeans2.df <- cbind(SRP.df$SRP.name,dailymeans.df)
-colnames(dailymeans2.df)[1] <- c("SRP.name")
-view(dailymeans2.df)
+#dailymeans2.df <- cbind(SRP.df$SRP.name,dailymeans.df)
+#colnames(dailymeans2.df)[1] <- c("SRP.name")
+#view(dailymeans2.df)
 
 #RENAME DF
 
@@ -84,15 +84,25 @@ print(time.end)
 
 ### subsetting the dataframe
 
-DD.df.cut <- DD.df[(DD.df$date >= time.start) & (DD.df$date <= time.end),]
+DD.df.cut <- dailymeans.df[(dailymeans.df$date >= time.start) & (dailymeans.df$date <= time.end),]
 
 view(DD.df.cut)
 
+
+##Add in metadata columns for final dataframe
+
+meta.df <- str_split_fixed(DD.df.cut$name, "_", n = 5)
+head(meta.df)
+
+DD.df.cut <- DD.df.cut %>% add_column(site = meta.df[,1], rep = meta.df[,2], position = meta.df[,3], buttonID = meta.df[,4], season = meta.df[,5],
+                         .before = "date")
+head(DD.df.cut)
+
 DDsums.df <- DD.df.cut %>% 
-    group_by(SRP.name) %>% 
-    summarise(SRP.name = SRP.name,
+    group_by(site, rep, position) %>% 
+    summarise(site = site, name = name, position = position,
               degree.days = sum(meantemp)) %>%  #create degree.days column by outputting the sum of meantemp for each position. (Not sensor, but position)
-    distinct(SRP.name, .keep_all = TRUE) %>% 
+    distinct(site, rep, position, .keep_all = TRUE) %>% 
     ungroup()
   view(DDsums.df)  
 
